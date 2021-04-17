3fe43e1f516d899556e19d8ab156aa52fd35827b
mphobwcodes <36450959+mphobwcodes@users.noreply.github.com>
2021-04-17 16:41:41 -0400

    First

---
+++ angularApp/src/app/components/post-form/post-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-form',
+  template: `
+    <p>
+      post-form works!
+    </p>
+  `,
+  styles: []
+})
+export class PostFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angularApp/src/app/components/post-list/post-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  template: `
+    <p>
+      post-list works!
+    </p>
+  `,
+  styles: []
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
